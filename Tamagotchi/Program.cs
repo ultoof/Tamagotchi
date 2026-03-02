d2f704ce61dae1d0bee624202140415edb294f75
ulti <neo.ljung@gmail.com>
2026-03-02 14:14:04 +0100

    first commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,33 @@
+﻿//-- Variables
+
+//-- Functions
+
+// Prompts the user to press anykey and waits using ReadKey().
+void PressAnyKey()
+{
+    Console.Write("\n> Press any key:");
+    Console.ReadKey();
+}
+
+// A method thats used when the user inputs something invalid.
+void ShowInvalidInput()
+{
+    Console.Clear();
+    Console.WriteLine("Invalid Input, Press any key");
+    Console.ReadKey();
+}
+
+// Waits for a user input with some fancy colors.
+string WaitForInput()
+{
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.Write("\n> Input: ");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    string inputedString = Console.ReadLine();
+    Console.ForegroundColor = ConsoleColor.White;
+    return inputedString;
+}
+
+//-- Runtime
+
+//-- Classes
