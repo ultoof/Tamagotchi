32831968c758b7a78b19d2d3d5f2f88e835b6a45
ulti <neo.ljung@gmail.com>
2026-05-18 13:45:03 +0200

    inshallah

--- Tamagotchi/obj/Debug/net9.0/Tamagotchi.AssemblyInfo.cs
+++ Tamagotchi/obj/Debug/net9.0/Tamagotchi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c17c0bfe2859fee45a62142ed6a4bec43555ed9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9464211beeeb5857c2bbaea5daa2ddc2b6b3c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tamagotchi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
